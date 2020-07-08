6dced1aad2f1de8a6222e914da8edfbeaf28dd4c
mikehoods <62457440+mikehoods@users.noreply.github.com>
2020-07-07 23:19:59 -0500

    mobile styling for about me

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,6 +14,7 @@
             <li>React</li>
             <li>MongoDB</li>
         </ul>
+        <img class="me" src="images/lookatme.png">
         <div class="links">
           <a href="https://www.linkedin.com/in/michaelhoods/"><img src="images/LI.png"></a>
           <a href="https://github.com/mikehoods"> <img src="images/github.png"></a>
@@ -41,7 +42,15 @@ export default {
     width: 100%;
     // opacity: 50%;
     border-radius: 12px 12px 0 0;
+    }
   }
+  .me {
+    max-width: 8vw;
+    border-radius: 50%;
+    border: 2px solid whitesmoke;
+    position: absolute;
+    left: 6%;
+    top: 8%;
   }
   .links {
     display: flex;
@@ -59,28 +68,46 @@ export default {
   }
   h1 {
     font-size: 2.1rem;
-    top: 10%;
+    top: 9%;
+    left: 20%
   }
   ul {
-    font-size: 1.4rem;
-    top: 35%;
-    left: 10%;
+    font-size: 1.6rem;
+    top: 30%;
+    left: 62%;
+    font-weight: 300;
     li {
       list-style: inside;
-      font-size: 1.2rem;
+      font-size: 1.4rem;
       padding: 0 0 0 2rem;
     }
   }
 }
-@media only screen and (max-width: 600px) {
+@media only screen and (max-width: 800px) {
   .card {
     .links {
       img {
         width: 6vw;
       }
     }
+    .me {
+      max-width: 12vw;
+      left: 2%;
+    }
     h1 {
       font-size: 1.5rem;
+      top: -6%
+    }
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      left: 2%;
+      font-size: 1.1rem;
+      li {
+        list-style: none;
+        font-size: 1.1rem;
+      }
     }
   }
 }
